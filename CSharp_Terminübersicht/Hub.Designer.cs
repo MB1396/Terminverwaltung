397c46718fcd37922dc702bad7910bc63ea9cbc9
MB1396 <m.brinkmann96@web.de>
2017-02-13 14:56:12 +0100

    Design Anpassungen

--- CSharp_Terminübersicht/Hub.Designer.cs
+++ CSharp_Terminübersicht/Hub.Designer.cs
@@ -73,6 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(386, 408);
             this.Controls.Add(this.end);
             this.Controls.Add(this.dateview);
